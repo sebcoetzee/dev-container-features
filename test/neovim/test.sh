c645b0d1089e892c2678618681e5194087688b9d
Sebastian Coetzee <seb.coetzee@gmail.com>
2025-01-01 22:40:32 +0000

    Add unit test for neovim

---
+++ test/neovim/test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+source dev-container-features-test-lib
+
+check "Neovim version" go version
+if ! env | grep --quiet '^PATH=.*\/nvim-linux64\/bin'; then
+	echo "~/nvim-linux64/bin not added to the PATH"
+fi
+
+reportResults
