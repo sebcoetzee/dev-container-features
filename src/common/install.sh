a46b3d345ab046152eedeca10166b842d9d51874
Sebastian Coetzee <seb.coetzee@gmail.com>
2025-01-02 01:18:04 +0000

    Compile latest version of tmux

--- src/common/install.sh
+++ src/common/install.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+
 set -eux
 
 echo "Installing common packages..."
 
 apt update
-apt install -y tmux stow wget
+apt install -y stow wget
+
+# tmux install from source
+apt install -y libevent-dev ncurses-dev build-essential bison pkg-config
+
+wget "https://github.com/tmux/tmux/releases/download/$TMUX_VERSION/tmux-$TMUX_VERSION.tar.gz"
+
+tar -xvf "tmux-$TMUX_VERSION.tar.gz"
+rm "tmux-$TMUX_VERSION.tar.gz"
+cd "tmux-$TMUX_VERSION"
+./configure
+make && make install
+
+cd ..
+rm -rf "tmux-$TMUX_VERSION"
 
