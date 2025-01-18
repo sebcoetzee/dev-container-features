781943d0b19ce0db5a58f43de87412bd4d65acf9
Sebastian Coetzee <seb.coetzee@gmail.com>
2025-01-18 12:40:56 +0000

    Add fzf to common

--- src/common/install.sh
+++ src/common/install.sh
@@ -4,10 +4,26 @@ export DEBIAN_FRONTEND=noninteractive
 
 set -eux
 
+ARCH="$(dpkg --print-architecture)"
+
 echo "Installing common packages..."
 
 apt update
-apt install -y stow wget
+apt install -y stow wget curl
+
+echo "Create .local directory..."
+
+mkdir -p $_REMOTE_USER_HOME/.local/bin
+mkdir -p $_REMOTE_USER_HOME/.local/lib
+mkdir -p $_REMOTE_USER_HOME/.local/include
+mkdir -p $_REMOTE_USER_HOME/.local/share
+
+echo "Installing fzf..."
+cd /tmp
+curl -L -o fzf.tar.gz https://github.com/junegunn/fzf/releases/download/v${FZF_VERSION}/fzf-${FZF_VERSION}-linux_${ARCH}.tar.gz
+tar -xvf fzf.tar.gz
+rm fzf.tar.gz
+mv fzf /usr/bin/
 
 # tmux install from source
 apt install -y libevent-dev ncurses-dev build-essential bison pkg-config
--- test/common/test.sh
+++ test/common/test.sh
@@ -4,6 +4,7 @@ set -e
 
 source dev-container-features-test-lib
 
+check "fzf version" fzf --version
 check "wget version" wget --version
 check "stow version" stow --version
 check "tmux version" tmux -V
