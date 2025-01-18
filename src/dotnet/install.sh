78dcb52588fd935e0cd69e2d23460a0b46af48d2
Sebastian Coetzee <seb.coetzee@gmail.com>
2025-01-18 21:41:13 +0000

    Fix issue with dotnet

--- src/dotnet/install.sh
+++ src/dotnet/install.sh
@@ -7,7 +7,7 @@ set -eux
 echo "Installing .NET $VERSION to $DOTNET_ROOT..."
 
 cd /tmp
-curl --fail -o dotnet.tar.gz -L https://builds.dotnet.microsoft.com/dotnet/Sdk/$VERSION/dotnet-sdk-6.0.428-linux-x64.tar.gz
+curl --fail -o dotnet.tar.gz -L https://builds.dotnet.microsoft.com/dotnet/Sdk/$VERSION/dotnet-sdk-$VERSION-linux-x64.tar.gz
 
 mkdir -p $DOTNET_ROOT
 tar -xvf dotnet.tar.gz -C $DOTNET_ROOT
